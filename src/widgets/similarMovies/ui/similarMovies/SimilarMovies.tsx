caeebaee6a5b367d8b03bfd9e725e5b1e2491cbd
Dmitriy Zakharov <dzaharov794@gmail.com>
2024-12-25 23:35:00 +0300

    fix

--- src/widgets/similarMovies/ui/similarMovies/SimilarMovies.tsx
+++ src/widgets/similarMovies/ui/similarMovies/SimilarMovies.tsx
@@ -12,8 +12,6 @@ const SimilarMovies = ({ data }: Props) => {
       <h3 className={styles.title}>Возможно, вам понравится</h3>
       {data.length > 0 ? <MovieList movies={data} /> : <div>Нет похожих фильмов</div>}
     </>
-
-
   );
 };
 
